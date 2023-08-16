cc6426b2ada52393bb971b581549793a371e4382
BraulioBarcenas <bar.brau6@utsoe.edu.mx>
2023-08-15 21:28:07 -0600

    Creacion del proyecto

---
+++ src/main/java/com/example/ListadoDeTareas/ListadoDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ListadoDeTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListadoDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListadoDeTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ListadoDeTareas/ListadoDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ListadoDeTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListadoDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
